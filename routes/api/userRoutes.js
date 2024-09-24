5a6bc405c8ddfabccc7505b8b822e8ffca5af829
Mitchell-610 <Mheit602@gmail.com>
2024-09-24 17:16:12 -0400

    Now we can access and use all get put and delete routes within user routes.

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -13,6 +13,13 @@ const {
 // /api/users
 router.route('/').get(getUsers).post(createUser);
 
+// /api/users/:userId
+router
+  .route('/:userId')
+  .get(getSingleUser)
+  .put(updateUser)
+  .delete(deleteUser);
+  
 
 
 module.exports = router;
