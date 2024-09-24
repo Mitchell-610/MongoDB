b4b501096a515d2631841554c5027ac24d8cc449
Mitchell-610 <Mheit602@gmail.com>
2024-09-24 17:06:31 -0400

    Now we can pull in just a users thoughts.

--- controllers/thoughtsController.js
+++ controllers/thoughtsController.js
@@ -72,4 +72,19 @@ module.exports = {
     }
   },
 
+  async getUserThoughts(req, res) {
+    const { userId } = req.params;
+    try {
+      const user = await User.findById(userId).populate('thoughts');
+      if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+      res.status(200).json(user.thoughts);
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  },
+
+ 
   
+};
